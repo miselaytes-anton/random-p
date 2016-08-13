b2c39faf0ab63a664b379b51057889ecd6de4d28
Anton Miselaytes <anton.miselaytes@philips.com>
2016-08-13 20:56:05 +0200

    google image search

---
+++ backend/lib/image/index.js
@@ -0,0 +1,29 @@
+const rp = require('request-promise'),
+  api_key = require('./api_key'),
+  _ = require('lodash')
+
+exports.get = word => {
+
+
+  const options = {
+    uri: 'https://www.googleapis.com/customsearch/v1',
+    qs: {
+      q: word,
+      searchType: 'image',
+      imgSize: 'medium',
+      num: 1
+    },
+    headers: {
+        'User-Agent': 'Request-Promise'
+    },
+    json: true // Automatically parses the JSON string in the response
+};
+  _.assign(options.qs, api_key);
+
+
+  return rp(options).then(res => ({
+    link: _.get(res, ['items', 0, 'link']),
+    title: _.get(res, ['items', 0, 'title'])
+  }));
+
+};
