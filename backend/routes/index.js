e342e3c037b6c4cf195a2666f61d26b9d5ba5eee
Anton Miselaytes <anton.miselaytes@philips.com>
2016-08-15 22:45:52 +0200

    hide 'older posts' if none are available

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,9 +8,16 @@ module.exports = app => {
         const page = parseInt(req.query.page) || 1;
         const postPerPage = 7;
 
-        return mdb.collection('posts').find({}).sort({_id: -1}).skip((page - 1) * postPerPage).limit(postPerPage)
-          .then(posts => res.render('index', {posts: posts, page: page}))
-          .catch(err => res.error(err));
+        return mdb.collection('posts').count({})
+          .then(count => mdb.collection('posts').find({}).sort({_id: -1}).skip((page - 1) * postPerPage).limit(postPerPage)
+            .then(posts => res.render('index', {
+              posts: posts,
+              page: page,
+              hasOlder: (count - page * postPerPage) > 0,
+              hasNewer: page > 1
+            }))
+            .catch(err => res.error(err)));
+
     });
 
     app.get('/api/posts', (req, res) =>
