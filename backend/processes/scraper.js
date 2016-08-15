048b77eee60e52a24ab2eee491a68b8a06ed86c0
Anton Miselaytes <anton.miselaytes@philips.com>
2016-08-15 22:28:03 +0200

    refactor scraper: prevent duplicates

--- backend/processes/scraper.js
+++ backend/processes/scraper.js
@@ -1,44 +1,60 @@
 const cron = require('node-cron'),
   _ = require('lodash'),
+  Promise = require('bluebird'),
   getWord = require('../lib/word').get,
   getImage = require('../lib/image').get,
   mdb = require('../models/mongo');
 
-
-const insert = () => {
-  // get random word
-  //get image for the word
-  // insert into mongo
-  return getPost().then(post => mdb.collection('posts').insert(post))
-    .catch(err => console.log(err));
-
-};
-
-const getPost = () => {
-  return getWord()
-    .then(word => getImage(word).then(image => {
-      if (image){
-        console.log(`${word} - ok`);
-
-        return _.assign(image, {word: word, date: new Date()});
-      } else {
-        console.log(`${word} - not found`);
-
-        return getPost();
+const getPost = () => getWord()
+  .then(word => mdb.collection('posts').findOne({word: word})
+    .then(post => post ? Promise.reject({word: word, code: 'word_duplicate'}) : getImage(word))
+    .then(image => {
+      if (!image) {
+        return Promise.reject({word: word, code: 'word_not_found'});
       }
 
-    }));
-};
+      return mdb.collection('posts').findOne({link: image.link})
+        .then(post => {
+          if (post) {
+            return Promise.reject({word: word, code: 'image_duplicate'});
+          }
 
-exports.schedule = () => {
-  console.log(`Hi, I'm a scraper. I generate content and insert into DB`);
+          return _.assign(image, {word: word, date: new Date()});
+        });
+    })
+);
 
-  // insert every hour
-  cron.schedule('0 0 * * *', () => {
-    console.log('Generating a post', new Date());
-    insert();
+
+const insert = () => getPost()
+  .then(post => {
+    console.log(`${post.word} - ok`);
+
+    return mdb.collection('posts').insert(post);
+  })
+  .catch(err => {
+
+    switch (err.code) {
+      case 'image_duplicate':
+      case 'word_duplicate':
+      case 'word_not_found':
+        console.error(err.word, '-', err.code);
+
+        return insert();
+      default:
+        console.error(err);
+    }
   });
+
+
+module.exports = {
+  schedule: () => {
+    console.log(`Hi, I'm a scraper. I generate content and insert into DB`);
+
+    // insert every day
+    cron.schedule('0 0 * * *', () => {
+      console.log('Generating a post', new Date());
+      insert();
+    });
+  },
+  insert: insert
 };
-
-exports.insert = insert;
-
