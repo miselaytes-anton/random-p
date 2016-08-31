105d84ae25ca09c53e92466d15e125d4dbd32b6d
Anton Miselaytes <anton.miselaytes@philips.com>
2016-08-31 19:19:00 +0300

    lib visual

---
+++ backend/lib/visual/index.js
@@ -0,0 +1,28 @@
+const rp = require('request-promise'),
+  api_key = require('./api_key'),
+  _ = require('lodash');
+
+
+module.exports.getClasses = url => {
+  const options = {
+    uri: 'https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify',
+    qs: {
+      url: url,
+      version: '2016-05-19'
+    },
+    headers: {
+      'User-Agent': 'Request-Promise'
+    },
+    json: true // Automatically parses the JSON string in the response
+  };
+  _.assign(options.qs, api_key);
+
+  return rp(options)
+    .then(res => {
+      //console.log(JSON.stringify(res));
+
+      return _.map(_.get(res, ['images', 0, 'classifiers', 0, 'classes']), 'class');
+    });
+};
+//http://www.ibm.com/watson/developercloud/visual-recognition/api/v3/?curl#classify_an_image
+//https://gateway-a.watsonplatform.net/visual-recognition/api/v3/classify?api_key={api_key}&url=http://www.metmuseum.org/toah/images/h5/h5_29.100.498_av1.jpg&version=2016-05-19
