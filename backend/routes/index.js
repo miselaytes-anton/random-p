539ca6b81c8958e09decdd31f8cdceac4f2f5dd9
Anton Miselaytes <anton.miselaytes@philips.com>
2016-09-01 15:53:15 +0300

    change how post is displayed

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,25 @@
 'use strict';
 
-const mdb = require('../models/mongo');
+const mdb = require('../models/mongo'),
+  _ = require('lodash');
+
+const capitalize = str => str.slice(0, 1).toUpperCase() + str.slice(1).toLowerCase();
+
+const formatPostForView = p => {
+  const msTags = _.get(p, ['msAnalysis', 'tags']) || [];
+  const ibmTags = _.get(p, ['ibmAnalysis', 'tags']) || [];
+
+  const roboCaption = p.msAnalysis && p.msAnalysis.caption ?
+    capitalize(p.msAnalysis.caption)
+    : 'I do not know';
+
+  return _.assign(p, {
+   // _title:  capitalize(p.word),
+    _tags: _.uniq(msTags.concat(ibmTags)),
+    _sourceCaption: p.image.title,
+    _roboCaption: roboCaption
+  });
+};
 
 
 module.exports = app => {
@@ -11,7 +30,7 @@ module.exports = app => {
         return mdb.collection('posts').count({})
           .then(count => mdb.collection('posts').find({}).sort({_id: -1}).skip((page - 1) * postPerPage).limit(postPerPage)
             .then(posts => res.render('index', {
-              posts: posts,
+              posts: posts.map(formatPostForView),
               page: page,
               hasOlder: (count - page * postPerPage) > 0,
               hasNewer: page > 1
@@ -27,3 +46,4 @@ module.exports = app => {
 
     app.get('*', (req, res) => res.status(404).json({error: 404, message: 'not found'}));
 };
+
