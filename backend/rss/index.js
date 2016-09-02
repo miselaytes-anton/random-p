f4a7581213e61801608a4ea3d568ee03cab44fb0
Anton Miselaytes <anton.miselaytes@philips.com>
2016-09-02 16:22:49 +0300

    rss: add word and tags

--- backend/rss/index.js
+++ backend/rss/index.js
@@ -3,6 +3,15 @@ const RSS = require('rss'),
   _ = require('lodash'),
   blog = require('../constants/blog');
 
+const getDescriptionHTML = p => `
+  <a href="${p.image.source}">
+    <img src="${p.image.link}">
+    <p>${p.image.title}</p>
+  </a>
+  <strong>Word</strong>: ${p.word}<br>
+  <strong>Tags</strong>: ${p._tags.join(', ')}
+`;
+
 module.exports = (req, res) => {
   const baseUrl = 'http://words.amiselaytes.com';
 
@@ -18,19 +27,19 @@ module.exports = (req, res) => {
     language: 'en',
     categories: ['Arts', 'Words', 'Robots'],
     pubDate: new Date(),
-    ttl: '60'
+    ttl: '1440'
   });
 
-  return mdb.collection('posts').find({}).sort({_id: -1}).limit(7)
+  return mdb.posts.find({}).sort({_id: -1}).limit(7)
     .then(posts => {
       /* loop over data and add to feed */
       _.each(posts, p => {
-        const tags = (_.get(p, 'msAnalysis.tags') || []).concat(_.get(p, 'ibmAnalysis.tags') || []);
+        const post = mdb.posts.formatForView(p);
         feed.item({
-          title:   _.get(p, 'msAnalysis.caption') || blog.defaultPostTitle,
-          description: `<a href="${p.image.source}"> <img src="${p.image.link}"> <p>${p.image.title}</p> </a>`,
+          title:   post._roboCaption,
+          description: getDescriptionHTML(p),
           url: `${baseUrl}/${p._id}`, // link to the item
-          categories: tags,
+          categories: p._tags,
           author: 'Robot', // optional - defaults to feed author property
           date: p.date // any format that js Date can parse.
 
